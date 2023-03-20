acf4c0390c99bff11ce9260b22d0d26878ae7cb0
mattia allegro <mattia.allegro@yahoo.it>
2023-03-20 22:41:30 +0100

    added setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name="py3-webpwn3r",
+    packages=["py3-webpwn3r"],
+    version="0.1",
+    license="Web Applications Security Scanner",
+    description="Web Applications Security Scanner",
+    author="ShadMod",
+    author_email="support@shadmod.it",
+    url="https://github.com/shadMod/py3-webpwn3r",
+    download_url="https://github.com/shadMod/py3-webpwn3r/archive/refs/tags/0.0.1.tar.gz",
+    keywords=[
+        "WebPwn3r",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
