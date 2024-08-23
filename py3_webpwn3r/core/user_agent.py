56e09ea3139ffb412fa715c2fff0bb8643755da2
mattia allegro <mattia.allegro@yahoo.it>
2024-08-23 18:35:25 +0200

    Fix imports

--- py3_webpwn3r/core/user_agent.py
+++ py3_webpwn3r/core/user_agent.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-from urllib.request import FancyURLopener, urlopen
+from urllib.request import FancyURLopener
 
 
 class UserAgent(FancyURLopener):
